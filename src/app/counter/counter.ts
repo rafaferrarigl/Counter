a772e823d08fcbdcad9fb861f0b64aefded5b10e
rafaferrarigl <yriveks1@gmail.com>
2025-06-22 12:02:20 +0200

    Add substract and reset

--- src/app/counter/counter.ts
+++ src/app/counter/counter.ts
@@ -4,8 +4,18 @@ import { Component } from '@angular/core';
   selector: 'app-counter',
   imports: [],
   templateUrl: './counter.html',
-  styleUrl: './counter.scss'
+  styleUrl: './counter.scss',
 })
 export class Counter {
+  counter: number = 0;
+  increment() {
+    this.counter++;
+  }
 
+  decrement() {
+    this.counter--;
+  }
+  reset() {
+    this.counter = 0;
+  }
 }
